6037d7ba8dc83b8819038e1c257f97216d87ae9a
Carolina Atampis <89546647+A-caro@users.noreply.github.com>
2022-04-21 13:02:41 -0300

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -31,13 +31,12 @@ CREATE TABLE Ejemplos (
     FOREIGN key (id_concepto) REFERENCES Conceptos(id_concepto)
 );
 
-select * from Conceptos
 
 insert into Conceptos
 (nombre_concepto,
 numero_concepto,
 contenido_concepto)
-values ('Declaracion e inicialización de variables', 1,  'Una variable es un nombre para una pieza de memoria que almacena
+values ('Declaracion e inicializaciÃ³n de variables', 1,  'Una variable es un nombre para una pieza de memoria que almacena
 datos.Una variable se declara indicando el tipo de dato 
 junto con un nombre, luego de esto le asignamos un valor para finalmente inicializarla.');
  
@@ -49,21 +48,21 @@ values ('Identificadores', 2,  'Los identificadores nombran variables, funciones
 
 
 insert into Preguntas(id_pregunta, contenido_pregunta, id_concepto)
-	values (1,'¿Que es una variable y cómo se declara?',4),
-	(2,'¿Cuales son las caracteristicas de un identificador?',5);
+	values (1,'Â¿Que es una variable y cÃ³mo se declara?',4),
+	(2,'Â¿Cuales son las caracteristicas de un identificador?',5);
 
 insert into Respuestas(id_respuesta, contenido_respuestas, id_pregunta, es_correcta)
 values (1,'Una variable es un nombre para una pieza de memoria que almacena
 datos.Una variable se declara indicando el tipo de dato junto con un nombre,
  luego de esto le asignamos un valor para finalmente inicializarla.',1, true),
-(2,' No pueden empezar por un dígito.No pueden contener ninguno de los caracteres especiales vistos en una entrada anterior. 
+(2,' No pueden empezar por un dÃ­gito.No pueden contener ninguno de los caracteres especiales vistos en una entrada anterior. 
 No puede ser una palabra reservada de Java. Las palabras reservadas en Java son todas las que aparecen en el punto siguiente.',2,true);
 
 
 INSERT INTO ejemplos(id_ejemplos, descripcion_ejemplo,contenido_ejemplo, id_concepto) 
 	VALUES (1,'Declaracion e inicializacion de variables','zooName = "The Best Zoo";
            numberAnimals = 100;',4),
-	(2,'Identificadores','edad, nombre, precio, año_nacimiento',5); 
+	(2,'Identificadores','edad, nombre, precio, aÃ±o_nacimiento',5); 
 
 
 
